a687b73fb201ca845b56a953e7f0619b3a828c78
massss____ <maxgmeinder07@gmail.com>
2025-02-23 22:20:13 +0100

    Dokumentation fertig

--- MASmessenger/MasMessenger_Backend/MasMessenger24/MasMessenger24/obj/Release/net8.0/MasMessenger24.AssemblyInfo.cs
+++ MASmessenger/MasMessenger_Backend/MasMessenger24/MasMessenger24/obj/Release/net8.0/MasMessenger24.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MasMessenger24")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f006649af776fb19469bbe57a1578fba23553e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MasMessenger24")]
